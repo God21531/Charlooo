958d8964c06d93584c7923a18794c3d99d815251
GodOfThunder <118043581+God21531@users.noreply.github.com>
2023-09-16 10:37:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+// Baidu Ao Tian October 10, 2019 https://github.com/AJLoveChina/birthday
+var config = {
+    // The length of sentences can be arbitrary, you can write ten or twenty sentences
+    // Try to keep each sentence within 15 characters to maintain a good display effect
+    texts: [
+        "To my",      
+        "beloved SweetCharlooo..!!",  
+        "Today it's your 20th birthday",
+        "Welcome Charloooo Aunty",
+        "Happy Birthday to the most loving person I have ever met.",
+        "You make me feel at home wherever I go.",
+        "As long as you are with me, I feel as safe as ever.",
+        "I pray",
+        "I should be wid you atleast till your 100th bday",
+        "but year, your more special to my heart",
+        "Let's celebrate together",
+        "Happy Birthday My Charloooo <3 ",
+        "GET ME DAIRY MILK BUBBY",
+        "Take Care Charloo",
+        "By your JOSH",
+    ],
+    /**
+     * You can leave the 'imgs' empty, but if you want to fill it in, it must follow the format below
+     * "Corresponding to the text above, must be exactly the same": "Image URL, you can put the image in the 'imgs' folder"
+     * For example:
+     * "To my beloved sweetheart": "./imgs/xiaokeai.jpg"
+     *
+     * If you don't want an image, just add two slashes at the beginning of each line like this, 
+     * for example, the image for "Today is your birthday" won't be displayed:)
+     * Tip: It's best to use square or nearly square images for a better display effect
+     */
+    imgs: {
+        "To my beloved sweetheart": "./imgs/xiaokeai.png",
+        // "Today is your birthday": "./imgs/birthday.jpg",
+    },
+    // Button text descriptions, the following are the default button texts in English, you can change them to your liking
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Colors",
+        balloons_flying: "Seems like something's missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
